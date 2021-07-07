bbbe771b231fb0dea081c015170f7a83192721b8
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-07-07 21:40:46 +0100

    windows deps issue

--- .github/workflows/scripts/release_windows.bat
+++ .github/workflows/scripts/release_windows.bat
@@ -3,5 +3,5 @@ echo on
 python -m pip install --upgrade pip
 pip install setuptools wheel twine auditwheel
 
-pip wheel . -w wheel/
+pip wheel . -w wheel/ --no-deps
 twine upload --skip-existing wheel/*
